9cb3335d5089b348fdd2123734bcbd982b1d493b
sushant2518 <sushantnarvekar3118@gmail.com>
2023-08-24 06:51:11 +0530

    day2

---
+++ StockDeal/src/main/java/com/masai/ui/MainUI.java
@@ -0,0 +1,97 @@
+package com.masai.ui;
+
+import java.util.Scanner;
+
+public class MainUI {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int choice;
+		do {
+
+			System.out.println("_____Enter selection _____");
+			System.out.println("|1. Login As Admin        |");
+			System.out.println("|2. Login As Customer     |");
+			System.out.println("|3. Customer Registration |");
+			System.out.println("|0. Exit                  |");
+			System.out.println("|_________________________|");
+			choice = sc.nextInt();
+
+			switch (choice) {
+			case 1:
+				LoginAsAdmin(sc);
+				break;
+			case 2:
+//				CustomerUI.userLogin(sc);
+				break;
+			case 3:
+//				CustomerUI.userRegistration(sc);
+				break;
+			case 0:
+				System.out.println("Thanks for using the services");
+				break;
+			default:
+				System.out.println("Invalid Selection, try again");
+			}
+		} while (choice != 0);
+
+	}
+
+	private static void LoginAsAdmin(Scanner sc) {
+		System.out.print("Enter username ");
+		String username = sc.next();
+		System.out.print("Enter password ");
+		String password = sc.next();
+		if (username.equals("admin") && password.equals("admin")) {
+			adminMenu(sc);
+		} else {
+			System.out.println("Invalid Username of Password");
+		}
+		
+	}
+	
+	
+	static void displayAdminMenu() {
+		System.out.println("_______________________________");
+		System.out.println("|1. Add New Stock             |");
+		System.out.println("|2. View All Stock            |");
+		System.out.println("|3. Update Stock Details      |");
+		System.out.println("|4. See List of all Customers |");
+		System.out.println("|5. Delete Stock              |");
+		System.out.println("|0. Logout                    |");
+		System.out.println("|_____________________________|");
+	}
+
+	public static void adminMenu(Scanner sc) {
+
+		int choice = 0;
+		do {
+			displayAdminMenu();
+			System.out.print("Enter selection ");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+//				AdminUI.addStock(sc);
+				break;
+			case 2:
+//				AdminUI.viewStocks(sc);
+				break;
+			case 3:
+//				AdminUI.updateStockDetails(sc);
+				break;
+			case 4:
+//				AdminUI.viewAllCustomers(sc);
+				break;
+			case 5:
+//				AdminUI.DeleteStockById(sc);
+				break;
+			case 0:
+				System.out.println("Bye Bye Admin");
+				break;
+			default:
+				System.out.println("Invalid Selection, try again");
+			}
+		} while (choice != 0);
+	}
+}
